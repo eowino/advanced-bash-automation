494f64c5c6b9e15b203b30d918b5e79217946f78
Evans Owino <evansowino@hotmail.com>
2019-03-16 13:24:28 +0000

    unzip and open-pr

---
+++ unzip.sh
@@ -0,0 +1,12 @@
+# extract file archive
+unzip() {
+    case "$1" in
+    *.tar|*.tgz) tar -xzvf "$1";;
+    *.gz) gunzip -k "$1";;
+    *.zip) unzip -v "$1";;
+    *)
+        echo "Cannot extract $1"
+        exit 1
+    ;;
+    esac
+}
